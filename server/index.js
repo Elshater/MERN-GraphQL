dadb99d2f3842f3ce65ada0449c0af1f90d2d389
elshater <elshater.ehab@gmail.com>
2023-03-18 18:05:20 +0200

    running graphql

--- server/index.js
+++ server/index.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 require('dotenv').config();
 
+const {graphqlHTTP} = require('express-graphql');
+
+
+const schema = require('./schema/schema');
+
 const port= process.env.PORT || 5000
 
 
 const app = express();
+
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: global,
+    graphiql: true,
+  }));
 
 app.listen(port, console.log(`Server listening on ${port}`))
